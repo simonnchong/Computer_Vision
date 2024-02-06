92adf2cb882b62233da341930b3c6b1a268774dc
Simon <simonnchong@gmail.com>
2024-02-06 17:07:57 +0800

    update

--- thresholding/global threshold.py
+++ thresholding/global threshold.py
@@ -10,7 +10,7 @@ ret, thresh1 = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)
 # ret, thresh1 = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY_TRUNC) #make the pixel over the threshold back to the threshold, otherwise remain the same
 # ret, thresh1 = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY_TOZERO) #remain the pixel if over the threshold, otherwise set to 0 ?(darkest)
 # ret, thresh1 = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY_TOZERO_INV) #remain the pixel if less than the threshold, otherwise set to 0 ?(darkest)
-print(ret)
+# print(ret)
 
 plt.imshow(thresh1, cmap="gray")
 plt.show()
